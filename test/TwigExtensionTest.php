32598b459ff62c83cfd84fbabf8fa175e7a981bd
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:48:41 -0600

    Rewriting as Laminas Project package

--- test/TwigExtensionTest.php
+++ test/TwigExtensionTest.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-twigrenderer for the canonical source repository
- * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-twigrenderer/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-twigrenderer for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-twigrenderer/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-twigrenderer/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Expressive\Twig;
+namespace MezzioTest\Twig;
 
+use Mezzio\Helper\ServerUrlHelper;
+use Mezzio\Helper\UrlHelper;
+use Mezzio\Twig\TwigExtension;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ProphecyInterface;
 use Twig_SimpleFunction as SimpleFunction;
-use Zend\Expressive\Helper\ServerUrlHelper;
-use Zend\Expressive\Helper\UrlHelper;
-use Zend\Expressive\Twig\TwigExtension;
 
 class TwigExtensionTest extends TestCase
 {
